89d985ede14db81ba7a64296e1d73e443fb5c785
Simon Belanger <simon-2.belanger@polymtl.ca>
2019-04-09 09:12:33 -0400

    keras final submission, CNN not conclusive since there is not enough data

--- RobotNotebook.py
+++ RobotNotebook.py
@@ -1,6 +1,7 @@
 # %%
 from __future__ import print_function
 
+import math
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,8 +10,6 @@ import tensorflow as tf
 
 from tensorflow import keras
 from sklearn.preprocessing import LabelEncoder
-
-from pylab import *
 
 logging.disable(sys.maxsize)
 
@@ -44,22 +43,51 @@ print(train_label["surface"].unique())
 print(train_label["group_id"].nunique())
 
 
+def feature_scaling(feature):
+    min_feature = feature.min()
+    max_feature = feature.max()
+    feature = feature.apply(lambda item: (item - min_feature) / (max_feature - min_feature))
+    return feature
+
+
+def quaternion_to_euler(dataframe):
+    x = dataframe["orientation_X"]
+    y = dataframe["orientation_Y"]
+    z = dataframe["orientation_Z"]
+    w = dataframe["orientation_W"]
+    roll_first_term = 2.0 * (w * x + y * z)
+    roll_second_term = 1 - 2.0 * (x * x + y * y)
+    dataframe["roll"] = np.arctan2(roll_first_term, roll_second_term)
+
+    pitch_term = 2.0 * (w * y - z * x)
+    dataframe["pitch"] = pitch_term.apply(lambda pitch: math.copysign(math.pi / 2, pitch) if math.fabs(pitch) >= 1 else math.asin(pitch))
+
+    yaw_first_term = 2.0 * (w * z + x * y)
+    yaw_second_term = 1.0 - 2.0 * (y * y + z * z)
+    dataframe["yaw"] = np.arctan2(yaw_first_term, yaw_second_term)    
+
+    return dataframe
+
+
 def transform_data(dataframe):
     transformed_dataframe = []
     temp_array = []
     index = 0
+    dataframe = quaternion_to_euler(dataframe)
+    dataframe["roll"] = feature_scaling(dataframe["roll"])
+    dataframe["pitch"] = feature_scaling(dataframe["pitch"])
+    dataframe["yaw"] = feature_scaling(dataframe["yaw"])
     dataframe_size = dataframe.shape[0]
     for i in range(dataframe_size):
         if dataframe["series_id"][i] != index:
             index += 1
             transformed_dataframe.append(np.asarray(temp_array))
-            temp_array = []
+            temp_array = []    
 
         temp_array.append([
-            dataframe["orientation_X"][i],
-            dataframe["orientation_Y"][i],
-            dataframe["orientation_Z"][i],
-            dataframe["orientation_W"][i],
+            dataframe["roll"][i],
+            dataframe["pitch"][i],
+            dataframe["yaw"][i],
             dataframe["angular_velocity_X"][i],
             dataframe["angular_velocity_Y"][i],
             dataframe["angular_velocity_Z"][i],
@@ -82,8 +110,8 @@ print(transformed_data.shape)
 # %%
 plt.figure(figsize=(26, 16))
 for i, col in enumerate(train_data.columns[3:]):
-    plt.subplot(3, 4, i + 1)
-    plt.plot(train_data.loc[train_data['series_id'] == 21, col])
+    plt.subplot(5, 3, i + 1)
+    plt.plot(train_data.loc[train_data['series_id'] == 15, col])
     plt.title(col)
 
 # %%
@@ -102,13 +130,16 @@ encoder.fit_transform(train_label["surface"])
 def train_model(
         training_examples,
         training_targets,
-        epoch
+        epoch,
+        use_dropout = False
 ):
 
     model = keras.Sequential([
-        keras.layers.Flatten(input_shape=(128, 10)),
-        keras.layers.Dense(156, activation=tf.nn.relu),
-        keras.layers.Dense(18, activation=tf.nn.relu),
+        keras.layers.Flatten(input_shape=(128, 9)),
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(64, activation=tf.nn.relu),
+        keras.layers.Dropout(0.3),
+        keras.layers.Dense(64, activation=tf.nn.relu),
         keras.layers.Dense(9, activation=tf.nn.softmax)
     ])
 
@@ -135,7 +166,7 @@ def evaluate_model(model):
 deep_classifier = train_model(
     training_examples=train_examples,
     training_targets=train_targets,
-    epoch=100
+    epoch=500
 )
 
 # %%
